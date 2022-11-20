929e18f77a417cf0ceb1d0803e34bad49e364ed3
naoki.sugai <musen1485@gmail.com>
2022-11-20 14:19:05 +0000

    add column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -54,7 +54,7 @@ class MessagesController < ApplicationController
 
     #Storong Parameter
     def message_params
-        params.require(:message).permit(:content)
+        params.require(:message).permit(:content, :title)
     end
 end
 
